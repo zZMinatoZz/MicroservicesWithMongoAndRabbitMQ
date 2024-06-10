2dc79f4a1bb30ddfc5e50111a96644747fc51c11
Nguyen Hoai Nam <heaven1xx3@gmail.com>
2024-06-10 15:48:14 +0700

    adding library Polly to implement request timeout, retry and circuit broker

--- Play.Inventory/src/Play.Inventory.Service/Program.cs
+++ Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -1,6 +1,8 @@
 using Play.Common.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
+using Polly;
+using Polly.Timeout;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +13,19 @@ builder.Services.AddHttpClient<CatalogClient>(client =>
 {
     // adding uri to send request to catalog service
     client.BaseAddress = new Uri("http://localhost:5236");
-});
+})
+// retry
+.AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(
+    5,
+    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+))
+// circuit breaker
+.AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+    3,
+    TimeSpan.FromSeconds(15)
+))
+// timeout
+.AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
